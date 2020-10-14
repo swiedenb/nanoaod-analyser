07227053214d8e04206897945555b4de31df8f36
Sebastian Wiedenbeck <sebastian.wiedenbeck@cern.ch>
2020-10-14 14:15:11 +0200

    more files pushed to have them backuped

--- include/Particles.cc
+++ include/Particles.cc
@@ -5,6 +5,8 @@
 // check if tau is in acceptance and fulfils id requirements
 rvec<bool> tau_acceptance_and_id(const rvec<float>& pt, 
                                  const rvec<float>& eta,
+                                 const rvec<float>& dz,
+                                 const rvec<int>& charge,
                                  const rvec<bool>& dm,
                                  const rvec<UChar_t>& iso, 
                                  const rvec<UChar_t>& antiEle_disc, 
@@ -14,6 +16,12 @@ rvec<bool> tau_acceptance_and_id(const rvec<float>& pt,
         
         // tau eta cut
         auto mask_eta = abs(eta) < config::tau_eta;
+        
+        // tau dz cut
+        auto mask_dz = abs(dz) < 0.2;
+        
+        // tau charge
+        auto mask_charge = charge != 0;
         
         // tau decay mode
         auto mask_dm = dm;
@@ -28,13 +36,13 @@ rvec<bool> tau_acceptance_and_id(const rvec<float>& pt,
         auto mask_antimu = (config::tau_antiMu_WP & antiMu_disc) == config::tau_antiMu_WP;
         
         // return vector with true, if particle fulfils all requirements - else false
-        return mask_pt & mask_eta & mask_dm & mask_iso & mask_antiele & mask_antimu;
+        return mask_pt & mask_eta & mask_charge & mask_dz & mask_dm & mask_iso & mask_antiele & mask_antimu;
 };
 
-// check if tau is in acceptance and fulfils id requirements with certain dm vetos
 rvec<bool> tau_acceptance_and_id_and_dm(const rvec<float>& pt, 
                                         const rvec<float>& eta,
                                         const rvec<float>& dz,
+                                        const rvec<int>& charge,
                                         const rvec<bool>& dm,
                                         const rvec<int>& dm_number,
                                         const rvec<UChar_t>& iso, 
@@ -48,23 +56,27 @@ rvec<bool> tau_acceptance_and_id_and_dm(const rvec<float>& pt,
         
         // tau dz cut
         auto mask_dz = abs(dz) < 0.2;
+        
+        // tau charge
+        auto mask_charge = charge != 0;
+        
         // tau decay mode
         auto mask_dm = dm;
 
         // tau decay mode as integer
         auto mask = not (dm_number == 5 || dm_number == 6);
                 
-        // tau iso requirement (1: VVLoose, 2: VLoose, 4: Loose, 8: Medium, 16: Tight, 32: VTight, 64: VVTight)
-        auto mask_iso = (iso & config::tau_iso_WP) == config::tau_iso_WP;
+        // tau iso requirement
+        auto mask_iso = (config::tau_iso_WP & iso) == config::tau_iso_WP;
         
-        // Anti Electron discriminator (1: VLoose, 2: Loose, 4: Medium, 8: Tight, 16: VTight)
+        // Anti Electron discriminator
         auto mask_antiele = (config::tau_antiE_WP & antiEle_disc) == config::tau_antiE_WP;
         
-        // Anti Muon discriminator (1: Loose, 2: Tight)
+        // Anti Muon discriminator
         auto mask_antimu = (config::tau_antiMu_WP & antiMu_disc) == config::tau_antiMu_WP;
         
         // return vector with true, if particle fulfils all requirements - else false
-        return mask_pt & mask_eta & mask_dz & mask & mask_dm & mask_iso & mask_antiele & mask_antimu;
+        return mask_pt & mask_eta & mask_dz & mask_charge & mask & mask_dm & mask_iso & mask_antiele & mask_antimu;
 };
 
 // check if event has a well seperated di muon pair
@@ -82,7 +94,7 @@ rvec<bool> di_muon_id(const rvec<float>& pt,
         auto mask_id = (id == config::muon_id_WP);
     
         // muon tracker iso 
-        auto mask_iso = iso / pt < 0.15;
+        auto mask_iso = iso < 0.15;
 
         
         // return vector with true, if particle fulfils all requirements - else false
@@ -93,7 +105,7 @@ rvec<bool> di_muon_id(const rvec<float>& pt,
 rvec<bool> muon_acceptance_and_id(const rvec<float>& pt, 
                                   const rvec<float>& eta, 
                                   const rvec<UChar_t>& id, 
-                                  const rvec<float>& iso) {
+                                  const rvec<UChar_t>& iso) {
         // muon pt cut
         auto mask_pt = pt > config::muon_pt;
         
@@ -103,8 +115,8 @@ rvec<bool> muon_acceptance_and_id(const rvec<float>& pt,
         // muon id
         auto mask_id = (id == config::muon_id_WP);
     
-        // muon relative iso 
-        auto mask_iso = (1 <= config::muon_iso_WP);
+        // muon relative iso id
+        auto mask_iso = (iso >= 1);
 
         
         // return vector with true, if particle fulfils all requirements - else false
@@ -152,7 +164,8 @@ rvec< rvec< float > > calc_tau_energy_scale(const rvec<int>& tau_decayMode,
                                             const rvec<float>& tau_pt,
                                             const rvec<float>& tau_eta,
                                             const rvec<float>& tau_phi,
-                                            const rvec<float>& tau_mass) {  
+                                            const rvec<float>& tau_mass,
+                                            std::string type = "") {  
         rvec<rvec<float>> binder(4);
         
         rvec<float> new_tau_pt(tau_pt.size());                          
@@ -165,13 +178,14 @@ rvec< rvec< float > > calc_tau_energy_scale(const rvec<int>& tau_decayMode,
             TLorentzVector p1; 
             p1.SetPtEtaPhiM(tau_pt[i], tau_eta[i], tau_phi[i], tau_mass[i]);
 
-            if (config::run_type == "_TauEnergyScaleUp")
-                p1 *= config::tau_dm_scale->getTES_highpt(tau_pt[i], tau_decayMode[i], (int) tau_genPartFlav[i], "Up");
-            else if (config::run_type == "Tau_EnergyScaleDown")
-                p1 *= config::tau_dm_scale->getTES_highpt(tau_pt[i], tau_decayMode[i], (int) tau_genPartFlav[i], "Down");
-            else
-                p1 *= config::tau_dm_scale->getTES_highpt(tau_pt[i], tau_decayMode[i], (int) tau_genPartFlav[i], "");
-                 
+            if (type == "_ESUp") {
+                p1 *= config::tau_dm_scale->getTES(tau_pt[i], tau_decayMode[i], (int) tau_genPartFlav[i], "Up");
+            } else if (type == "_ESDown") {
+                p1 *= config::tau_dm_scale->getTES(tau_pt[i], tau_decayMode[i], (int) tau_genPartFlav[i], "Down");
+            } else {
+                p1 *= config::tau_dm_scale->getTES(tau_pt[i], tau_decayMode[i], (int) tau_genPartFlav[i], "");
+            }
+
             new_tau_pt[i] = p1.Pt();
             new_tau_eta[i] = p1.Eta();
             new_tau_phi[i] = p1.Phi();
--- include/Particles.hh
+++ include/Particles.hh
@@ -15,6 +15,8 @@ using RNode = ROOT::RDF::RNode;
 
 rvec<bool> tau_acceptance_and_id(	const rvec<float>& pt, 
                                     const rvec<float>& eta, 
+                                    const rvec<float>& dz,
+                                    const rvec<int>& charge, 
                                     const rvec<bool> & dm,
                                     const rvec<UChar_t>& iso, 
                                     const rvec<UChar_t>& antiEle_disc, 
@@ -25,6 +27,7 @@ rvec<bool> tau_acceptance_and_id(	const rvec<float>& pt,
 rvec<bool> tau_acceptance_and_id_and_dm(	const rvec<float>& pt, 
                                             const rvec<float>& eta, 
                                             const rvec<float>& dz,
+                                            const rvec<int>& charge,
                                             const rvec<bool>& dm,
                                             const rvec<int>& dm_number, 
                                             const rvec<UChar_t>& iso, 
@@ -36,7 +39,7 @@ rvec<bool> tau_acceptance_and_id_and_dm(	const rvec<float>& pt,
 rvec<bool> muon_acceptance_and_id(	const rvec<float>& pt, 
 									const rvec<float>& eta, 
 									const rvec<UChar_t>& id, 
-									const rvec<float>& iso
+									const rvec<UChar_t>& iso
 									);
 
 rvec<bool> di_muon_id(const rvec<float>& pt, 
@@ -56,7 +59,8 @@ rvec< rvec< float > > calc_tau_energy_scale(const rvec<int>& tau_decayMode,
                                             const rvec<float>& tau_pt,
                                             const rvec<float>& tau_eta,
                                             const rvec<float>& tau_phi,
-                                            const rvec<float>& tau_mass);
+                                            const rvec<float>& tau_mass,
+                                            std::string type);
 
 rvec <float> apply_tau_energy_scale(const rvec< rvec<float> > binder, 
                                     const int& index);
